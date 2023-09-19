110fd28106fda583808fb731793d06a1f4e5b51f
Chase Palmer <chase.palmer012704@gmail.com>
2023-09-18 23:48:42 -0400

    General formatting for portfolio stats and minor tweaking

--- src/components/PortfolioStats/PortfolioStats.jsx
+++ src/components/PortfolioStats/PortfolioStats.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './PortfolioStats.css'
 
 const PortfolioStats = () => {
     return (
-        <div className="outer">
+        <div className="outer" style={{display:"flex"}}>
+            <div className="inner-box">
+                <h3 style={{borderBottom: "2px solid #606266"}}>Portfolio Value</h3>
+                <h3 className="num">$1103</h3>
+            </div>
+            <div className="inner-box">
+                <h3 style={{borderBottom: "2px solid #606266"}}>Net Change</h3>
+                <h3 className="num">+11.48</h3>
+            </div>
         </div>
     )
 }
