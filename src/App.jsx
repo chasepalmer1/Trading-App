dffe5b1638d9b64fb828afbd8bb67a462934dad7
Chase Palmer <chase.palmer012704@gmail.com>
2023-09-19 21:21:36 -0400

    Merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="signUp" element/>
-        <Route path="/paperTrade" element={<PaperTrading/>}/>
+        <Route path="/" element={<PaperTrading/>}/>
         <Route path="/portTrack" element={<PortfolioTracking/>}/>
 
         <Route path="/login" element={<Login/>}/>
