3b5d975745ea454367e1411f1d4fe06462e60e5f
Chase Palmer <chase.palmer012704@gmail.com>
2023-10-24 18:29:47 -0400

    :wrench: uhhhh

--- src/components/GeneralIndices/GeneralIndices.jsx
+++ src/components/GeneralIndices/GeneralIndices.jsx
@@ -1,29 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './GeneralIndices.css'
 
 import IndexTrack from '../IndexTrack/IndexTrack'
 import axios from 'axios'
 import { API_PORT } from '../../../api/models/constants'
 
-const GeneralIndices = async() => {
+const GeneralIndices = () => {
 
-    const ticker = "aapl";
+    const [list, setList] = useState([]);
+    useEffect (() => {
+        const fetchData = async() => {
+            let l = []
 
-    try {
-        const resp = await axios.post(`http://localhost:${API_PORT}/finance/quote`, {'ticker': ticker}, {headers: {"Content-Type": "application/json"}})
-        console.log(resp.body)
-    }
-    catch(E) {
-        console.log(E)
-    }
+            const tickers = ["aapl", "goog", "voo"]
+            for (let ticker of tickers) {
+                
+                try {
+                    const resp = await axios.post(`http://localhost:${API_PORT}/finance/quote`, {'ticker': ticker}, {headers: {"Content-Type": "application/json"}})
+                    console.log(resp.data)
+                    l.push({key: resp.data.name, price: `\$${resp.data.price}`})
+                }
+                catch(E) {
+                    console.log(E)
+                }
+        
+            }
+            setList(l)
+        }
+        fetchData().catch(e => console.log(e))
+    }, [list, setList])
 
 
     return (
         <div class="outer outer-gen">
-            <IndexTrack class="major-index" ticker="S&P 500 Index" price="4309.7" dayGain="65.08" dayGainPerc="3.05"/>
-            <IndexTrack class="major-index" ticker="S&P 500 Index" price="4309.7" dayGain="65.08" dayGainPerc="3.05"/>
-            <IndexTrack class="major-index" ticker="S&P 500 Index" price="4309.7" dayGain="65.08" dayGainPerc="3.05"/>
-            <IndexTrack class="major-index" ticker="S&P 500 Index" price="4309.7" dayGain="65.08" dayGainPerc="3.05"/>
+            {
+                list.map((index) => {
+                    return <IndexTrack class="major-index" ticker={index.key} price={index.price}/>
+                })
+            }
 
         </div>
     )
